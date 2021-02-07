96804e0b6a402bf401ef07aefa6ebf7bf0307289
EC2 Default User <ec2-user@ip-172-31-88-86.ec2.internal>
2021-02-07 04:34:09 +0000

    add script header

---
+++ src/show-portal-unvisited.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @id             iitc-plugin-show-portal-unvisited
+// @name           IITC plugin: show portal unvisited
+// @category       Highlighter
+// @version        0.0.1.20210207.18214
+// @namespace      https://github.com/nikola-f/iitc-plugins
+// @downloadURL    https://github.com/nikola-f/iitc-plugins/raw/master/dist/show-portal-unvisited.js
+// @homepageURL    https://github.com/nikola-f/iitc-plugins
+// @description Use the fill color of the portals to show if the portal is unvisited or uncaptured.
+// @author nikola-f
+// @include        https://intel.ingress.com/*
+// @include        http://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
+// @match          http://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
