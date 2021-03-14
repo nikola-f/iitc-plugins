5e89712b60d1367979536feeba13373aefebd851
nikola-f <ec2-user@ip-172-31-88-86.ec2.internal>
2021-03-14 12:17:00 +0000

    add options dialog

--- src/show-portal-unvisited.user.js
+++ src/show-portal-unvisited.user.js
@@ -28,36 +28,97 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 // use own namespace for plugin
 window.plugin.portalUnvisited = function() {};
 
+window.plugin.portalUnvisited.STORAGE_KEY_PREFIX = 
+  `plugin-show-portal-unvisited/${window.PLAYER.nickname}/`;
+
+
 window.plugin.portalUnvisited.highlightUnvisited = function(data)  {
   if(map.getZoom() < 15) {
     return;
   }
   
   var ent = data.portal.options.ent;
-  var style = {
-    fillColor: data.portal.fillColor,
-    fillOpacity: 1.0
+
+  var history = {
+    visited: false,
+    captured: false
   };
-
   if(ent && ent[2] && ent[2][18]) {
-
-    if(ent[2][18] === 1) { // visited = uncaptured
-      style.fillColor = 'red';
-
-    }else if(ent[2][18] === 3){ // captured
-      style.opacity = 0.5;
-      style.fillOpacity = 0.00;
-    }
-
-  }else{ // unvisited
-    style.fillColor = 'purple';
+    history.visited = !!(ent[2][18] & 1);
+    history.captured = !!(ent[2][18] & 2);
   }
+
+  if(!history.visited) {
+    data.portal.setStyle({
+      fillColor: 'purple',
+      fillOpacity: 1
+    });
+  }else if(!history.captured){
+    data.portal.setStyle({
+      fillColor: 'red',
+      fillOpacity: 1
+    });
+  }else{
+    data.portal.setStyle({
+      opacity: 0.5,
+      fillOpacity: 0
+    });
+  }
+
+};
+
+
+window.plugin.portalUnvisited.enableCaching = false;
+
+
+window.plugin.portalUnvisited.showOption = function() {
+
+  const html = `
+    <div>
+      <center>
+        <label id="enable-caching-toggle">
+          <input type="checkbox" name="enable-caching"
+          onchange="window.plugin.portalUnvisited.enableCaching = 
+                    !window.plugin.portalUnvisited.enableCaching; return false;"
+          ${window.plugin.portalUnvisited.enableCaching ? "checked" : ""} />
+          Enable caching history on your browser
+        </label>
+        <br />
+        <a onclick="window.plugin.drawTools.optCopy();" tabindex="0">Reset Cached History</a>
+      </center>
+    </div>
+  `;
   
-  data.portal.setStyle(style);
+  dialog({
+    html: html,
+    id: 'plugin-show-portal-unvisited-option',
+    title: 'Portal Unvisited Options',
+    close: function() {
+      window.localStorage.setItem(
+        window.plugin.portalUnvisited.STORAGE_KEY_PREFIX + 'enable-caching',
+        window.plugin.portalUnvisited.enableCaching
+      );
+    }
+  })
+
 };
 
 
 var setup =  function() {
+
+  const enableCachingString = 
+    window.localStorage.getItem(window.plugin.portalUnvisited.STORAGE_KEY_PREFIX + 'enable-caching');
+  if(enableCachingString && enableCachingString === "true") {
+    window.plugin.portalUnvisited.enableCaching = true;
+  }
+
+  $('<a>')
+    .html('PortalUnvisited Opt')
+    .attr('title','[x]')
+    .click(window.plugin.portalUnvisited.showOption)
+    .appendTo('#toolbox');
+
+  
   window.addPortalHighlighter('Portal Unvisited', window.plugin.portalUnvisited.highlightUnvisited);
 };
 // PLUGIN END //////////////////////////////////////////////////////////
